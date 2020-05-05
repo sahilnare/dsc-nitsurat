6a48f8b22834b95a1e8608c76b7bf83b1b978c5b
Sahil Nare <sahilnare78@gmail.com>
2020-05-05 15:50:34 +0530

    Added features and their css transitions

--- client/src/pages/LandingPage/LandingPage.js
+++ client/src/pages/LandingPage/LandingPage.js
@@ -5,14 +5,20 @@ import Footer from "../../components/Footer/Footer";
 import Logo from "../../img/dsc.jpg";
 import ReactTypingEffect from "react-typing-effect";
 import { Container, Row, Col } from "react-bootstrap";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 import "./LandingPage.css";
 
 class LandingPage extends Component {
 
+	componentDidMount() {
+		AOS.init();
+	}
+
 	render() {
 		return (
-			<div className="LandingPage">
+			<div className="Landing">
 				<Navbars />
 				<Container fluid className="LandingPage">
 				  <Row className="dsc-body">
@@ -32,6 +38,36 @@ class LandingPage extends Component {
 							</h1>
 						</Col>
 				  </Row>
+					<Row className="dsc-features">
+						<Col className="feature-image" xs={4} data-aos="fade-right" data-aos-offset="400">
+							<img src={Logo} className="feature-design" alt="dsc-nitsurat" />
+						</Col>
+						<Col className="feature-description" data-aos="fade-left" data-aos-offset="400">
+							<h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+							Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+							Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h3>
+						</Col>
+					</Row>
+					<Row className="dsc-features">
+						<Col className="feature-description" data-aos="fade-right" data-aos-offset="400">
+							<h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+							Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+							Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h3>
+						</Col>
+						<Col className="feature-image" xs={4} data-aos="fade-left" data-aos-offset="400">
+							<img src={Logo} className="feature-design" alt="dsc-nitsurat" />
+						</Col>
+					</Row>
+					<Row className="dsc-features">
+						<Col className="feature-image" xs={4} data-aos="fade-right" data-aos-offset="400">
+							<img src={Logo} className="feature-design" alt="dsc-nitsurat" />
+						</Col>
+						<Col className="feature-description" data-aos="fade-left" data-aos-offset="400">
+							<h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+							Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+							Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h3>
+						</Col>
+					</Row>
 				</Container>
 				<Footer />
 			</div>
